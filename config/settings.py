209953c4d3f270c66a146640671a5fd34dd8a7f8
JSSatorres <juansataz.dev@gmail.com>
2024-11-10 12:22:06 +0100

    chore3: connection db settings

--- config/settings.py
+++ config/settings.py
@@ -6,5 +6,8 @@ load_dotenv()
 
 class Settings(BaseSettings):
     mongo_uri: str = os.getenv("MONGO_URI")
+    comunio_password: str = os.getenv("COMUNIO_PASSWORD")
+    comunio_user: str = os.getenv("COMUNIO_USER")
+    
 
 settings = Settings()
