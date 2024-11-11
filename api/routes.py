073f15200327da48d86080dc3d3202f9fabeae65
JSSatorres <juansataz.dev@gmail.com>
2024-11-11 10:59:55 +0100

    chore: new routes

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from scraper.scraper import get_dashboard_data
+from services.player_service import player_service
+from api.api_utils import transform_object_id
 
 router = APIRouter()
 
@@ -15,3 +17,21 @@ def scrape_dashboard():
     
     except Exception as e:
         return {"success": False, "error": str(e)}
+    
+@router.get("/players")
+async def get_players():
+    players = player_service.get_all_players()
+    # Convertir cada ObjectId en los documentos
+    return [transform_object_id(player) for player in players]
+
+@router.post("/players")
+async def add_player(player_data: dict):
+    player_service.add_player(player_data)
+    return {"success": True}
+
+@router.get("/players/{name}")
+async def get_player_by_name(name: str):
+    player = player_service.find_player_by_name(name)
+    if player:
+        return transform_object_id(player)
+    return {"error": "Player not found"}
