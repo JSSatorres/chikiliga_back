76217c61a54b9ccf8de9e46bdc3aa3b90e99b1fe
JSSatorres <juansataz.dev@gmail.com>
2025-02-23 10:56:54 +0100

    feat: add PlayerRepository interface for player data management

---
+++ context/player/domain/player_repository.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from typing import Optional, List
+from .player_model import Player
+
+class PlayerRepository(ABC):
+    @abstractmethod
+    def get_by_id(self, player_id: str) -> Optional[Player]:
+        pass
+
+    @abstractmethod
+    def save(self, player: Player) -> Player:
+        pass
+
+    @abstractmethod
+    def get_all(self) -> List[Player]:
+        pass
