438ad2273c2119acdb9a3dbf66fbbc8943c752c3
JSSatorres <juansataz.dev@gmail.com>
2025-02-26 11:56:08 +0100

    feat: add empty __init__.py files for utils and shared directories

---
+++ app/utils/__init__.py
@@ -0,0 +1,3 @@
+# FILE: /fastapi-ddd-hexagonal/fastapi-ddd-hexagonal/app/utils/__init__.py
+
+# This file is intentionally left blank.
---
+++ shared/__init__.py
@@ -0,0 +1,3 @@
+# FILE: shared/__init__.py
+
+# This file is intentionally left blank.
