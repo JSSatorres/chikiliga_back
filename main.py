641d2c96bf0271101e3d5f3747c9cf0046068f1f
JSSatorres <juansataz.dev@gmail.com>
2025-03-04 18:27:54 +0100

    feat: integrate player, team, and market controllers into main application

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import uvicorn
 from fastapi import FastAPI
-from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+from context.player.presentation.player_controller import PlayerController
+from context.team.presentation.team_controller import TeamController
+from context.market.presentation.market_controller import MarketController
 from starlette.responses import RedirectResponse
 
-# from context.teams.infrastructure.routes import team_router
-# from context.players.infrastructure.routes import player_router
-
 app = FastAPI(
-    title="Football Stats API",
-    description="API to fetch football player stats from Comunio and Comuniazo",
-    version="1.0"
+  title="Football Stats API",
+  description="API to fetch football player stats from Comunio and Comuniazo",
+  version="1.0"
 )
+
+player_controller = PlayerController()
+team_controller = TeamController()
+market_controller = MarketController()
 
 @app.get("/", include_in_schema=False)
 async def root():
     return RedirectResponse(url="/docs")
 
-# app.include_router(team_router, prefix="/teams", tags=["Teams"])
-app.include_router(player_router, prefix="/players", tags=["Players"])
+app.include_router(player_controller.router, prefix="/players", tags=["players"])
+app.include_router(team_controller.router, prefix="/teams", tags=["teams"])
+app.include_router(market_controller.router, prefix="/markets", tags=["markets"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
