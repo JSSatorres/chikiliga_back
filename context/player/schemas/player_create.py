5d44df0d58161916b7edba869ae4a1bbbb422d2e
JSSatorres <juansataz.dev@gmail.com>
2025-05-07 19:14:32 +0200

    fix: change last_5_games_goals and last_5_games_assists types from List[int] to int in PlayerCreate schema

--- context/player/schemas/player_create.py
+++ context/player/schemas/player_create.py
@@ -15,5 +15,5 @@ class PlayerCreate(BaseModel):
     team_win_percentage: float = Field(..., description="Team's win percentage")
     goals: int = Field(..., description="Total number of goals scored")
     assists: int = Field(..., description="Total number of assists")
-    last_5_games_goals: List[int] = Field(..., description="Goals scored in last 5 games")
-    last_5_games_assists: List[int] = Field(..., description="Assists made in last 5 games")
+    last_5_games_goals: int = Field(..., description="Goals scored in last 5 games")
+    last_5_games_assists: int = Field(..., description="Assists made in last 5 games")
